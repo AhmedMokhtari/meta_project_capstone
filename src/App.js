3c382c43876c6ab7d5d7e45387bb51bbf5346252
mokhtari <mokhtari.ahmed21@ump.ac.ma>
2023-04-17 00:22:58 +0000

    update index page with nav and hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer/Footer';
+import Hero from './component/Hero/Hero';
+import Highlights from './component/Highlights/Highlights';
+import MyNavbar from './component/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      
+        <MyNavbar></MyNavbar>
+        <Hero></Hero>
+        <Highlights></Highlights>
+        <Footer></Footer>
     </div>
   );
 }
